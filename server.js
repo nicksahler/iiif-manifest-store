3d71f93dc7accf095030323788382c8a82b957f8
Nick Sahler <nicksahler@users.noreply.github.com>
2019-10-24 23:29:28 -0400

    more cleanup

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var fs = require('fs');
-var uuid = require('uuid');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const uuid = require('uuid')
 
 // Create app
-var app = express();
+const app = express()
 
 /*
 Route                         HTTP Verb Description
@@ -18,98 +20,79 @@ Route                         HTTP Verb Description
 */
 
 // ## CORS middleware
-// 
+//
 // see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
 // app.use(express.methodOverride());
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-      
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.sendStatus(200);
-    }
-    else {
-      next();
-    }
-};
-app.use(allowCrossDomain);
+function cleanPath(filePath) {
+  return path.normalize(filePath).replace(/^(\.\.(\/|\\|$))+/, '')
+}
 
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-app.use(bodyParser.json({ limit: '50mb' }));
+function formatPath(filePath) {
+  return `data/manifests/${cleanPath(filePath)}`
+}
 
-app.route('/api/manifests')
+app.use(cors())
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
+app.use(bodyParser.json({ limit: '50mb' }))
+
+app
+  .route('/api/manifests')
   // list all manifets
   .get(function(req, res) {
-    // look up manifest list on the file system
-    var manifestFiles = fs.readdirSync('data/manifests');
+    const files = fs.readdirSync('data/manifests')
 
-    var manifestUris = [];
-    manifestFiles.map((manifestFilename, index) => {
-      manifestUris.push({
-        uri: 'https://' + req.headers.host + '/m/api/manifests/' + manifestFilename
-      })
-    });
+    const manifests = files.map(filename => ({
+      uri: `https://${req.headers.host}/m/api/manifests/${filename}`,
+    }))
 
-    // generate JSON list with manifest uris
-    res.json({ manifests: manifestUris });
+    res.json({ manifests })
   })
 
   // create a manifest
   .post(function(req, res) {
-    // create a unique id for the manifest
-    var manifestId = uuid();
+    const id = uuid()
+    fs.writeFileSync(formatPath(id), JSON.stringify(req.body))
+    res.status(201)
 
-    // store the manifest on the file system
-    fs.writeFileSync('data/manifests/' + manifestId, JSON.stringify(req.body));
+    const uri = `https://${req.headers.host}/m/api/manifests/${id}`
+    res.json({ uri })
+  })
 
-    // set the status code in the response
-    res.status(201);
-
-    // return the manifest uri
-    res.json({ uri: 'https://' + req.headers.host + '/m/api/manifests/' + manifestId });
-  });
-
-app.route('/api/manifests/:manifestId')
+app
+  .route('/api/manifests/:manifestId')
   // get manifest with id
   .get(function(req, res) {
-    // get the manifest from the file system
-    var manifestData = fs.readFileSync('data/manifests/' + req.params.manifestId, 'utf8');
-
-    // return the manifest data
-    res.json(JSON.parse(manifestData));
+    const data = fs.readFileSync(formatPath(req.params.manifestId), 'utf8')
+    res.json(JSON.parse(data))
   })
 
   // update an existing manifest with id
   .put(function(req, res) {
-    var manifestPath = 'data/manifests/' + req.params.manifestId;
-    var statusCode = 200;
+    const path = formatPath(req.params.manifestId)
+    var statusCode = 200
 
     // check the file system to determine whether the resource exists
-    if(fs.existsSync(manifestPath)) {
-      // overwrite the manifest data on the file system
-      fs.writeFileSync(manifestPath, JSON.stringify(req.body));
+    if (fs.existsSync(path)) {
+      fs.writeFileSync(path, JSON.stringify(req.body))
     } else {
-      statusCode = 404;
+      statusCode = 404
     }
 
-    // set the status code in the response
-    res.status(statusCode);
-    res.json({ message: 'Manifest successfully updated' });
+    res.status(statusCode)
+    res.json({ message: 'Manifest successfully updated' })
   })
 
   // delete an existing manifest with id
   .delete(function(req, res) {
-    // Note: this is currently not implemented - we need authentication for this
-    res.json({ errorMessage: 'Deleting manifests is currently not supported' });
-  });
+    // TODO: Rudimentary authentication
+    res.json({ errorMessage: 'Deleting manifests is currently not supported' })
+  })
 
 // set port
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
 // listen on port
-app.listen(PORT, function () {
-  console.log('IIIF manifest store server is up on port ' + PORT);
-});
+app.listen(PORT, function() {
+  console.log('IIIF manifest store server is up on port ' + PORT)
+})

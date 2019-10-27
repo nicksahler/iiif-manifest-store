65a7673ff56150560bed569101d8bfc95495260f
Nick Sahler <nicksahler@users.noreply.github.com>
2019-10-27 18:22:03 -0400

    Add namespaces and runtime args

--- server.js
+++ server.js
@@ -5,31 +5,75 @@ const fs = require('fs')
 const path = require('path')
 const uuid = require('uuid')
 
-// Create app
+const argv = require('yargs')
+  .alias('p', 'path')
+  .describe('p', 'path where data will be stored')
+  .default('p', './data')
+  .alias('n', 'namespaces')
+  .describe(
+    'n',
+    'Optional namespaces to separate up data. Use as a list ( -n a -n b )'
+  )
+  .default('n', ['stage', 'prod'])
+  .help('help').argv
+
+for (let p of argv.namespaces) {
+  const dir = path.resolve(argv.path, p)
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true })
+    console.log(`Creating data directory '${dir}'`)
+  }
+}
+
 const app = express()
 
 /*
 Route                         HTTP Verb Description
 -------------------------------------------------------------------------------------------
-/api/manifests                GET       Get all manifests
-/api/manifests                POST      Create a manifest - returns manifest uri
-/api/manifests/:manifestId    GET       Get manifest by id
-/api/manifests/:manifestId    PUT       Update manifest with id
-/api/manifests/:manifestId    DELETE    Delete manifest with id (currently not implemented)
+/:namespace/manifests               GET       Get all manifests
+/:namespace/manifests               POST      Create a manifest - return manifest uri
+/:namespace/manifests/:mid   GET       Get manifest by id
+/:namespace/manifests/:mid   PUT       Update manifest with id
+/:namespace/manifests/:mid   DELETE    Delete manifest with id (currently not implemented)
 -------------------------------------------------------------------------------------------
 */
-
-// ## CORS middleware
-//
-// see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
-// app.use(express.methodOverride());
-
-function cleanPath(filePath) {
+const cleanPath = filePath => {
   return path.normalize(filePath).replace(/^(\.\.(\/|\\|$))+/, '')
 }
 
-function formatPath(filePath) {
-  return `data/manifests/${cleanPath(filePath)}`
+const formatPath = (namespace, id = '') => {
+  return path.resolve(argv.path, namespace, id)
+}
+
+const verifyNamespaceMiddleware = (req, res, next) => {
+  if (!argv.namespaces.includes(req.params.namespace))
+    return res
+      .status(403)
+      .json({ message: `Invalid namespace '${req.params.namespace}'` })
+
+  next()
+}
+
+const cleanManifestIdMiddleware = (req, res, next) => {
+  req.params.mid = cleanPath(req.params.mid)
+  next()
+}
+
+const getFullPathMiddleware = (req, res, next) => {
+  req.fullManifestPath = formatPath(req.params.namespace, req.params.mid)
+
+  if (!fs.existsSync(req.fullManifestPath))
+    return res
+      .status(404)
+      .json({
+        message: `Manifest '${req.params.mid}' does not exist in namespace '${req.params.namespace}'`,
+      })
+
+  next()
+}
+
+const formatManifestURI = (host, namespace, id) => {
+  return `https://${host}/m/${namespace}/manifests/${id}`
 }
 
 app.use(cors())
@@ -37,62 +81,57 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 app.use(bodyParser.json({ limit: '50mb' }))
 
 app
-  .route('/api/manifests')
+  .route('/:namespace/manifests')
   // list all manifets
-  .get(function(req, res) {
-    const files = fs.readdirSync('data/manifests')
+  .all(verifyNamespaceMiddleware)
+  .get((req, res) => {
+    const filePath = formatPath(req.params.namespace)
+    const files = fs.readdirSync(filePath)
 
     const manifests = files.map(filename => ({
-      uri: `https://${req.headers.host}/m/api/manifests/${filename}`,
+      uri: formatManifestURI(req.headers.host, req.params.namespace, filename),
     }))
 
     res.json({ manifests })
   })
-
   // create a manifest
-  .post(function(req, res) {
+  .post((req, res) => {
     const id = uuid()
-    fs.writeFileSync(formatPath(id), JSON.stringify(req.body))
-    res.status(201)
+    const uri = formatManifestURI(req.headers.host, req.params.namespace)
 
-    const uri = `https://${req.headers.host}/m/api/manifests/${id}`
-    res.json({ uri })
+    fs.writeFileSync(
+      formatPath(request.params.namespace, id),
+      JSON.stringify(req.body)
+    )
+    res.status(201).json({ uri })
   })
 
 app
-  .route('/api/manifests/:manifestId')
+  .route('/:namespace/manifests/:mid')
+  .all(verifyNamespaceMiddleware)
+  .all(cleanManifestIdMiddleware)
+  .all(getFullPathMiddleware)
   // get manifest with id
-  .get(function(req, res) {
-    const data = fs.readFileSync(formatPath(req.params.manifestId), 'utf8')
+  .get((req, res) => {
+    const data = fs.readFileSync(req.fullManifestPath, 'utf8')
     res.json(JSON.parse(data))
   })
-
   // update an existing manifest with id
-  .put(function(req, res) {
-    const path = formatPath(req.params.manifestId)
-    var statusCode = 200
-
-    // check the file system to determine whether the resource exists
-    if (fs.existsSync(path)) {
-      fs.writeFileSync(path, JSON.stringify(req.body))
-    } else {
-      statusCode = 404
-    }
-
-    res.status(statusCode)
-    res.json({ message: 'Manifest successfully updated' })
+  .put((req, res) => {
+    fs.writeFileSync(req.fullManifestPath, JSON.stringify(req.body))
+    res.status(status).json({ message: 'Manifest successfully updated' })
   })
 
   // delete an existing manifest with id
-  .delete(function(req, res) {
+  .delete((req, res) => {
     // TODO: Rudimentary authentication
-    res.json({ errorMessage: 'Deleting manifests is currently not supported' })
+    res
+      .status(404)
+      .json({ message: 'Deleting manifests is currently not supported' })
   })
 
 // set port
 const PORT = process.env.PORT || 3001
 
 // listen on port
-app.listen(PORT, function() {
-  console.log('IIIF manifest store server is up on port ' + PORT)
-})
+app.listen(PORT, () => console.log(`IIIF manifest store server | port ${PORT}`))
